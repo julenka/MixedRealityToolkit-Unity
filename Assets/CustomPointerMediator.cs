cc631f603d14720ac31dbc37100bdcc042aede14
Julia Schwarz <julenka@cs.cmu.edu>
2019-06-03 16:32:10 -0700

    Code to turn far pointers on/off

---
+++ Assets/CustomPointerMediator.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class CustomPointerMediator : DefaultPointerMediator
+{
+    public bool FarPointersDisabled {get; set;}
+
+    public override void UpdatePointers()
+    {
+        base.UpdatePointers();
+        if (FarPointersDisabled)
+        {
+            foreach(var pointer in farInteractPointers)
+            {
+                pointer.IsActive = false;
+            }
+        }
+    }
+}
---
+++ Assets/PointerConfigurationExample.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class PointerConfigurationExample : MonoBehaviour
+{
+    /* Turns off all far interaction pointers */
+    public void TurnOffFarPointers()
+    {
+        Debug.Log("Line pointers off");
+        SetFarPointersDisabled(true);
+    }
+
+    public void TurnOnFarPointers()
+    {
+        Debug.Log("Line pointers on");
+        SetFarPointersDisabled(false);
+    }
+
+    private void SetFarPointersDisabled(bool isDisabled)
+    {
+        FocusProvider focusProvider = (FocusProvider) MixedRealityToolkit.InputSystem.FocusProvider;
+        if (focusProvider != null)
+        {
+            foreach(var mediator in focusProvider.PointerMediators)
+            {
+                // Note: you could check here to make sure you only disable pointers for hands
+                CustomPointerMediator myMediator = (CustomPointerMediator) (mediator.Value);
+                if (myMediator != null)
+                {
+                    myMediator.FarPointersDisabled = isDisabled;
+                }
+            }
+        }
+    }
+
+}
